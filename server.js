323e21f20dd8b2fa22b40a28aae07cd1b02e7f6d
akshay <akshay.singhal@neosoftmail.com>
2023-03-30 14:38:10 +0530

    server

--- server.js
+++ server.js
@@ -4,17 +4,13 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config('');
 
-const mongodbconnect = require('../server/Connection/mongoose');
+const mongodbconnect = require('./Connection/mongoose');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 mongodbconnect();
 app.use(cors());
-app.use(express.static('./build'));
 app.use("/",routes);
 const path = require('path');
-app.get('*', (req, res) => {
-res.sendFile(path.resolve(__dirname, './build', 'index.html'));
-});
 const PORT = process.env.PORT || 3000;
 console.log('server started on port:',PORT);
 app.listen(PORT);
